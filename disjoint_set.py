6c3e242bd7ba2a7698c39b35484d6d866fd0f7f4
CAELO <83531146+CAEL0@users.noreply.github.com>
2021-07-20 21:50:50 +0900

    Update disjoint_set.py

--- disjoint_set.py
+++ disjoint_set.py
@@ -1,5 +1,9 @@
 """
-In computer science, a disjoint-set data structure, also called a union–find data structure or merge–find set, is a data structure that stores a collection of disjoint (non-overlapping) sets. Equivalently, it stores a partition of a set into disjoint subsets. It provides operations for adding new sets, merging sets (replacing them by their union), and finding a representative member of a set. The last operation allows to find out efficiently if any two elements are in the same or different sets.
+In computer science, a disjoint-set data structure, also called a union–find data structure or merge–find set, 
+is a data structure that stores a collection of disjoint (non-overlapping) sets. Equivalently, it stores a partition 
+of a set into disjoint subsets. It provides operations for adding new sets, merging sets (replacing them by their 
+union), and finding a representative member of a set. The last operation allows to find out efficiently if any two 
+elements are in the same or different sets.
 """
 
 
