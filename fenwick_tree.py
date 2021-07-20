f1ceeb612e875a36cd25e7fbdc1b3cb00341e717
CAELO <83531146+CAEL0@users.noreply.github.com>
2021-07-20 22:14:12 +0900

    Create fenwick_tree.py

---
+++ fenwick_tree.py
@@ -0,0 +1,20 @@
+"""
+A Fenwick tree or binary indexed tree is a data structure that can efficiently update elements and calculate 
+prefix sums in a table of numbers.
+"""
+
+
+class FenwickTree:
+    def __init__(self, array):
+        self.array = array
+
+    def update(self, idx, value):
+        while idx < len(self.array):
+            self.array[idx] += value
+            idx += idx & ~idx
+
+    def summation(self, idx):
+        res = 0
+        while idx >= 0:
+            res += self.array[idx]
+            idx -= idx & ~idx
