40e576dfbb6d641b573f6c0b2fc3eb2716d69977
Jongmin Kim <kjongmin26@gmail.com>
2021-10-02 17:56:48 +0900

    Update DoublyLinkedList.java

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -4,47 +4,89 @@ public class DoublyLinkedList<E> {
         private E element;
         private Node<E> next;
         private Node<E> prev;
+        
         public Node(E e, Node<E> n, Node<E> p) {
             element = e;
             next = n;
             prev = p;
         }
-        public E getElement() { return element; }
-        public Node<E> getNext() { return next; }
-        public void setNext(Node<E> n) { next = n; }
-        public Node<E> getPrev() { return prev; }
-        public void setPrev(Node<E> p) { prev = p; }
+        
+        public E getElement() {
+            return element;
+        }
+        
+        public Node<E> getNext() {
+            return next;
+        }
+        
+        public void setNext(Node<E> n) {
+            next = n;
+        }
+        
+        public Node<E> getPrev() {
+            return prev;
+        }
+        
+        public void setPrev(Node<E> p) {
+            prev = p;
+        }
     }
+    
     // Variables
     private Node<E> head = null;
     private Node<E> tail = null;
     private int size = 0;
+    
+    // Constructor
     public DoublyLinkedList() { }
-    public int size() { return size; }
+    
     // Methods
-    public boolean isEmpty() { return size == 0; }
+    public int size() {
+        return size;
+    }
+    
+    public boolean isEmpty() { 
+        return size == 0;
+    }
+    
     public E first() {
-        if (isEmpty()) return null;
+        if (isEmpty()) {
+            return null;
+        }
         return head.getElement();
     }
+    
     public E last() {
-        if (isEmpty()) return null;
+        if (isEmpty()) {
+            return null;
+        }
         return tail.getElement();
     }
+    
     public void appendLeft(E e) {
         Node<E> node = new Node<E>(e, head, null);
-        if (isEmpty()) tail = node;
-        else head.setPrev(node);
+        if (isEmpty()) {
+            tail = node;
+        }
+        else {
+            head.setPrev(node);
+        }
         head = node;
         size++;
     }
+    
     public void append(E e) {
         Node<E> node = new Node<E>(e, null, tail);
-        if (isEmpty()) head = node;
-        else tail.setNext(node);
+        if (isEmpty()) {
+            head = node;
+        }
+        else {
+            tail.setNext(node);
+        }
         tail = node;
         size++;
     }
+    
     public void popLeft() {
         if (!isEmpty()) {
             head = head.getNext();
@@ -52,6 +94,7 @@ public class DoublyLinkedList<E> {
             size--;
         }
     }
+    
     public void pop() {
         if (!isEmpty()) {
             tail = tail.getPrev();
@@ -59,28 +102,38 @@ public class DoublyLinkedList<E> {
             size--;
         }
     }
+    
     public void insertAfter(Node<E> n, E e) {
         Node<E> tar = head;
         while (tar != null) {
             if (tar == n) {
                 Node<E> node = new Node<E>(e, tar.getNext(), tar);
                 tar.setNext(node);
-                if (node.getNext() == null) tail = node;
-                else node.getNext().setPrev(node);
+                if (node.getNext() == null) {
+                    tail = node;
+                }
+                else {
+                    node.getNext().setPrev(node);
+                }
                 size++;
                 break;
             }
             tar = tar.getNext();
         }
     }
+    
     public void deleteAfter(Node<E> n) {
         Node<E> tar = head;
         while (tar != null) {
             if (tar == n) {
                 if (tar.getNext() != null) {
                     tar.setNext(tar.getNext().getNext());
-                    if (tar.getNext() == null) tail = tar;
-                    else tar.getNext().setPrev(tar);
+                    if (tar.getNext() == null) {
+                        tail = tar;
+                    }
+                    else {
+                        tar.getNext().setPrev(tar);
+                    }
                     size--;
                 }
                 break;
