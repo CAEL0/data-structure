204fa5c241bbe0677af2dfa93de96c8f117185d1
Jongmin Kim <kjongmin26@gmail.com>
2021-10-04 00:00:49 +0900

    Create LinkedQueue.java

---
+++ java/queue/LinkedQueue.java
@@ -0,0 +1,17 @@
+package queue;
+import linked_list.SinglyLinkedList;
+
+/** Realization of a FIFO queue as an adaptation of a SinglyLinkedList. */
+public class LinkedQueue<E> implements Queue<E> {
+    private SinglyLinkedList<E> list = new SinglyLinkedList<>();    // an empty list
+    
+    // constructor
+    public LinkedQueue() { }         // new queue relies on the initially empty list
+    
+    // methods
+    public int size() { return list.size(); }
+    public boolean isEmpty() { return list.isEmpty(); }
+    public void enqueue(E element) { list.addLast(element); }
+    public E first() { return list.first(); }
+    public E dequeue() { return list.removeFirst(); }
+}
