9f3b23b648453cba66441ec71f22f72586088c46
Jongmin Kim <kjongmin26@gmail.com>
2021-10-04 00:29:15 +0900

    Update SinglyLinkedList.java

--- java/SinglyLinkedList.java
+++ java/SinglyLinkedList.java
@@ -1,133 +1,112 @@
-public class SinglyLinkedList<E> {
-    // Node Class
-    private static class Node<E> {
-        private E element;
-        private Node<E> next;
+package linked_list;
 
+public class SinglyLinkedList<E> {
+    //---------------- nested Node class ----------------
+    public static class Node<E> {
+        private E element;              // reference to the element stored at this node
+        private Node<E> next;           // reference to the subsequent node in the list
         public Node(E e, Node<E> n) {
             element = e;
             next = n;
         }
+        public E getElement() { return element; }
+        public Node<E> getNext() { return next; }
+        public void setNext(Node<E> n) { next = n; }
+    } //----------- end of nested Node class -----------
 
-        public E getElement() {
-            return element;
-        }
-
-        public Node<E> getNext() {
-            return next;
-        }
-
-        public void setNext(Node<E> n) {
-            next = n;
-        }
-    }
-
-    // Variables
-    private Node<E> head = null;
-    private Node<E> tail = null;
-    private int size = 0;
+    // instance variables
+    private Node<E> head = null;        // head node of the list (or null if empty)
+    private Node<E> tail = null;        // last node of the list (or null if empty)
+    private int size = 0;               // number of nodes in the list
     
-    // Constructor
-    public SinglyLinkedList() {
-    }
+    // constructor
+    public SinglyLinkedList() { }       // constructs an initially empty list
 
-    // Methods
-    public int size() {
-        return size;
-    }
-
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    public E first() {
-        if (isEmpty()) {
-            return null;
-        }
+    // access methods
+    public int size() { return size; }
+    public boolean isEmpty() { return size == 0; }
+    public E first() {                  // returns (but does not remove) the first element
+        if (isEmpty()) return null;
         return head.getElement();
     }
-
-    public E last() {
-        if (isEmpty()) {
-            return null;
-        }
+    public E last() {                   // returns (but does not remove) the last element
+        if (isEmpty()) return null;
         return tail.getElement();
     }
 
-    public void appendLeft(E e) {
-        Node<E> node = new Node<E>(e, head);
-        if (isEmpty()) {
-            tail = node;
-        }
-        head = node;
+    // update methods
+    public void addFirst(E e) {         // adds element e to the front of the list
+        head = new Node<>(e, head);     // create and link a new node
+        if (isEmpty())
+            tail = head;                // special case: new node becomes tail also
         size++;
     }
-
-    public void append(E e) {
-        Node<E> node = new Node<E>(e, null);
-        if (isEmpty()) {
-            head = node;
+    public void addLast(E e) {          // adds element e to the end of the list
+        Node<E> newest = new Node<>(e, null);   // node will eventually be the tail
+        if (isEmpty())
+            head = newest;              // special case: previously empty list
+        else
+            tail.setNext(newest);       // new node after existing tail
+        tail = newest;                  // new node becomes the tail
+        size++;
+    }
+    public E removeFirst() {            // removes and returns the first element
+        if (isEmpty()) return null;     // nothing to remove
+        E answer = head.getElement();
+        head = head.getNext();          // will become null if list had only one node
+        size--;
+        if (size == 0)
+            tail = null;                // special case as list is now empty
+        return answer;
+    }
+    public E removeLast() {
+        if (isEmpty()) return null;
+        E answer = tail.getElement();
+        Node<E> node = head;
+        while (node.getNext().getNext() != null) {
+            node = node.getNext();
         }
-        else {
-            Node<E> tar = head;
-            while (tar.getNext() != null) {
-                tar = tar.getNext();
-            }
-            tar.setNext(node);
-        }
+        node.setNext(null);
         tail = node;
-        size++;
+        size--;
+        return answer;
     }
-
-    public void popLeft() {
-        if (!isEmpty()) {
-            head = head.getNext();
-            size--;
-        }
-    }
-
-    public void pop() {
-        if (!isEmpty()) {
-            Node<E> tar = head;
-            while (tar.getNext().getNext() != null) {
-                tar = tar.getNext();
-            }
-            tar.setNext(null);
-            tail = tar;
-            size--;
-        }
-    }
-
     public void insertAfter(Node<E> n, E e) {
         Node<E> tar = head;
         while (tar != null) {
             if (tar == n) {
                 Node<E> node = new Node<E>(e, tar.getNext());
                 tar.setNext(node);
-                if (node.getNext() == null) {
+                if (node.getNext() == null)
                     tail = node;
-                }
                 size++;
                 break;
             }
             tar = tar.getNext();
         }
     }
-
     public void deleteAfter(Node<E> n) {
         Node<E> tar = head;
         while (tar != null) {
             if (tar == n) {
                 if (tar.getNext() != null) {
                     tar.setNext(tar.getNext().getNext());
-                    if (tar.getNext() == null) {
+                    if (tar.getNext() == null)
                         tail = tar;
-                    }
                     size--;
                 }
                 break;
             }
             tar = tar.getNext();
+        }
+    }
+    public void reverse(Node<E> n) {
+        if (n != null && n.getNext() != null) {
+            Node<E> tmp = head;
+            head = n.getNext();
+            n.setNext(head.getNext());
+            head.setNext(tmp);
+            reverse(n);
         }
     }
 }
